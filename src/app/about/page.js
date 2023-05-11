6852f8f05c4601223da3eaedc0be2d45721064f4
Maximiliam Reimertz <max.reimertz@gmail.com>
2023-05-12 00:12:49 +0200

    added more styling

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -3,27 +3,25 @@
 import styles from "../styles/about.module.css";
 import Link from "next/link";
 
-
 const About = () => {
 	return (
-	
-			<main className={styles.about_main}>
-			<section className={styles.information} >
+		<main className={styles.about_main}>
+			<section className={styles.information}>
 				<div className={styles.personal_info}>
-					<div className={styles.personal_wrapper}>
-				<h1>Maximiliam Reimertz</h1>
-				<h3>smth smth</h3>
-				<div className={styles.contact_info}>
-					<Link href={"https://github.com/mxrmrtz"}>GitHub</Link>
-					<Link href={"https://www.linkedin.com/in/max-reimertz-83a272221/"}>Linkdin</Link>
-					<Link href={"https://twitter.com/mxrmrtz"}>Twitter</Link>
-					<Link href={"https://www.instagram.com/mxrmrtz/"}>Instagram</Link>
-					</div>
+						<h1>Maximiliam Reimertz</h1>
+						<h3>smth smth</h3>
+						<div className={styles.contact_info}>
+							<Link href={"https://github.com/mxrmrtz"}>GitHub</Link>
+							<Link href={"https://www.linkedin.com/in/max-reimertz-83a272221/"}>Linkdin</Link>
+							<Link href={"https://twitter.com/mxrmrtz"}>Twitter</Link>
+							<Link href={"https://www.instagram.com/mxrmrtz/"}>Instagram</Link>
+						</div>
 				</div>
-				</div>
-		
 			</section>
-			</main>
+			<section>
+				
+			</section>
+		</main>
 	);
 };
 
