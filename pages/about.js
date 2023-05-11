79e19165c02360660d068e141e97e75d23c01142
Maximiliam Reimertz <max.reimertz@gmail.com>
2023-05-11 14:13:08 +0200

    added about page

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+import { useRouter } from "next/router";
+
+const About = () => {
+	const router = useRouter();
+
+	return <div>sadad</div>;
+};
+
+export default About;
---
+++ src/app/components/3D_components/3dhero/HelixCanvas.js
@@ -0,0 +1,15 @@
+"use client"
+
+import Spline from "./Spline"
+import { Suspense } from 'react'
+import { Canvas } from '@react-three/fiber'
+
+export default function Home({}) {
+  return (
+    <Suspense fallback={null}>
+    <Canvas style={{ width: '100', height: '100vh' }} shadows flat linear>
+      <Spline />
+    </Canvas>
+  </Suspense>
+  )
+}
